2dc6c7b99bbbd93114356113b0a5d49018a1a4fe
Kevin Yanuk <fugwenn@gmail.com>
2018-06-12 20:04:31 -0400

    role check

--- src/bunkbot.py
+++ src/bunkbot.py
@@ -409,9 +409,9 @@ class BunkBot(commands.Bot):
                     await bunk_user.update_xp(0.1)
                     await self.remove_roles(bunk_user.member, self.role_streaming)
 
-                    if bunk_user.is_vip and not bunk_user.has_role(self.role_vip.name):
+                    if bunk_user.is_vip:
                         await self.add_roles(bunk_user.member, self.role_vip)
-                    elif bunk_user.is_moderator and not bunk_user.has_role(self.role_moderator.name):
+                    elif bunk_user.is_moderator:
                         await self.add_roles(bunk_user.member, self.role_moderator)
 
         except BunkException as be:
