0f47e216ba50f8f4d083fd2b70cee597a00488e7
fugwenna <fugwenna@gmail.com>
2020-05-10 11:54:59 -0400

    xkcd cog

--- src/core/http.py
+++ src/core/http.py
@@ -4,12 +4,14 @@ from urllib.request import HTTPError, URLError, socket
 from .bunk_exception import BunkException
 
 # make a basic http call
-async def http_get(self, url: str) -> json:
+async def http_get(url: str) -> json:
     try:
-        return json.loads(urllib.request.urlopen(url, timeout=1).read())
+        http_result = urllib.request.urlopen(url, timeout=1).read()
+        return json.loads(http_result)
     except socket.timeout:
         raise BunkException("http timeout")
     except (HTTPError, URLError) as uhe:
+        print(uhe)
         raise uhe
     except Exception as e:
         raise e
